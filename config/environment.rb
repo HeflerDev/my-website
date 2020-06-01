a33c8bdb0e971ae29e544599cd0acc8d8fece2fd
HeflerDev <hheflerdevelopment@gmail.com>
2020-06-01 11:01:57 -0300

    Configure Mailing

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
 # Load the Rails application.
 require_relative 'application'
 
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'hheflerdevelopment@gmail.com',
+  :password => 'Slyther.1n',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 465,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only:[:new, :create]
+  get '/contact', to: 'users#new'
+  post '/contact', to: 'users#create'
+
   root 'pages#home'
   get '/about', to: 'pages#about_me'
-  get '/contact', to: 'pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
