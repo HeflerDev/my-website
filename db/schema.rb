787eb2a8e1a71443c6ffb206c973a2986d5d727e
HeflerDev <hheflerdevelopment@gmail.com>
2020-06-03 12:30:54 -0300

    Adjust Database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_05_29_204946) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "text"
